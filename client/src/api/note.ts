8c8200f0317b6226c55d2a3552e4781dbf616f8b
Luxxyyy <alfjoseph.silao@gmail.com>
2025-09-18 08:32:38 +0800

    note fix

--- client/src/api/note.ts
+++ client/src/api/note.ts
@@ -1,11 +1,20 @@
 import http from './http';
 
+type NewNoteData = {
+  title: string;
+  message: string;
+  image?: string | null; 
+  isDone?: boolean;
+  latitude: number;
+  longitude: number;
+};
+
 export const getNotes = async () => {
   const response = await http.get("/notes");
   return response.data;
 };
 
-export const addNote = async (noteData) => {
+export const addNote = async (noteData: NewNoteData) => {
   const response = await http.post("/notes", noteData);
   return response.data;
 };
